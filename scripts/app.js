8f6e2a87375872baee221f716398576597c9f901
George McCadden III <thethirdone.g@gmail.com>
2020-12-05 17:56:37 -0800

    Just more practice

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,17 @@
 alert("It is actually working!");
 
-// This is an example 
+// This is an example of a performance review
 
-let rating = 3;
+// 3 - superstar
+// 2 - meets expectations
+// 1 - needs improvement
+
+let rating = 1;
 
 if (rating === 3) {
     console.log(`You are a superstar! Your rating is ${rating}!`);
+} else if (rating === 2) {
+    console.log(`You meet expectations! Woohoo! Your rating is ${rating}.`);
+} else if (rating === 1) {
+    console.log(`You need to improve. Your rating is ${rating}..`);
 }
