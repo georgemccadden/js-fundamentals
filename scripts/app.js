057c1aa5dcd26595cfefd483cc2768f0dd18a8d4
George McCadden III <thethirdone.g@gmail.com>
2020-12-05 17:51:10 -0800

    Just more logging

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-alert("It is working!");
+alert("It is actually working!");
+
+// This is an example 
+
+let rating = 3;
+
+if (rating === 3) {
+    console.log(`You are a superstar! Your rating is ${rating}!`);
+}
