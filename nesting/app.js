ba37404f0d41ddfd38f32b61a58fd94056d81d16
George McCadden III <thethirdone.g@gmail.com>
2020-12-07 06:56:22 -0800

    Using a truthy conditional

--- nesting/app.js
+++ nesting/app.js
@@ -1,10 +1,18 @@
-let password = 'Wishingy0uam3rrylittl3christmas!';
+let password = 'ThisIsMyPassword!';
+let validPassword = false;
 
 // Checking to see if it is a valid password
 if (password.length >= 6) {
 	if (password.indexOf(' ') === -1) {
 		console.log(`Valid Password`);
+		validPassword = true;
 	}
 } else {
 	console.log(`Password is not long enough!`);
+}
+
+// Automatically logging in
+
+if (validPassword) {
+	console.log(`You are currently logged in!`);
 }
