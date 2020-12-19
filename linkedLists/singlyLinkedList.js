e1d0d19b9b2dbb3cbf78940895ecd597f3cc3423
George McCadden III <thethirdone.g@gmail.com>
2020-12-19 01:02:18 -0800

    We will see if I am correct
    My only worry is whether or not the new node needs to be stored in a value or not

--- linkedLists/singlyLinkedList.js
+++ linkedLists/singlyLinkedList.js
@@ -17,4 +17,18 @@ class SinglyLinkedList {
 		this.tail = null;
 		this.length = 0;
 	}
+	push(val) {
+		let newbie = new Node(val);
+
+		if (!this.head) {
+			this.head = newbie;
+			this.tail = newbie;
+			this.length++;
+		} else {
+			this.tail.next = newbie;
+			newbie = this.tail;
+			newbie.next = null;
+			this.length++;
+		}
+	}
 }
