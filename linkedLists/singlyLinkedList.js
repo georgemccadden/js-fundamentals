4e79990ef8115360e7429a938b20b3822f658aa6
George McCadden III <thethirdone.g@gmail.com>
2020-12-19 00:54:38 -0800

    Creating a node

--- linkedLists/singlyLinkedList.js
+++ linkedLists/singlyLinkedList.js
@@ -3,3 +3,10 @@
 A data structure which contains a `head`, `tail`, and a `length` property. They consist of nodes and each of them has a value and a pointer to another node. (Best for insertions and deletions)
 
 */
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
