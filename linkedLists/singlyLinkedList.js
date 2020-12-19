848b4a2a6d203cdeb9d83b2400b5058c535219f7
George McCadden III <thethirdone.g@gmail.com>
2020-12-19 00:38:03 -0800

    Linked Lists

---
+++ linkedLists/singlyLinkedList.js
@@ -0,0 +1,5 @@
+/*
+
+A data structure which contains a `head`, `tail`, and a `length` property. They consist of nodes and each of them has a value and a pointer to another node. (Best for insertions and deletions)
+
+*/
--- nesting/app.js
+++ nesting/app.js
@@ -1,12 +1,10 @@
-let password = 'ThisIsMyPassword!';
+let password = 'DoesThisWork?';
 let validPassword = false;
 
 // Checking to see if it is a valid password
-if (password.length >= 6) {
-	if (password.indexOf(' ') === -1) {
-		console.log(`Valid Password`);
-		validPassword = true;
-	}
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+	console.log(`Valid Password`);
+	validPassword = true;
 } else {
 	console.log(`Password is not long enough!`);
 }
