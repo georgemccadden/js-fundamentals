e0fc76b6897a1126a4be3f0f20a81b3329443b85
George McCadden III <thethirdone.g@gmail.com>
2021-02-19 22:03:08 -0800

    Just doing a little binary searching

--- trees/binarySearchTrees.js
+++ trees/binarySearchTrees.js
@@ -5,3 +5,34 @@ class Node {
 		this.right = null;
 	}
 }
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+	insert(value) {
+		const newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode;
+		} else {
+			let curr = this.root;
+			while (true) {
+				if (value < curr.value) {
+					if (!curr.left) {
+						curr.left = newNode;
+						return this;
+					}
+					curr = curr.left;
+				} else {
+					if (value > curr.value) {
+						if (!curr.right) {
+							curr.right = newNode;
+							return this;
+						}
+						curr = curr.right;
+					}
+				}
+			}
+		}
+	}
+}
