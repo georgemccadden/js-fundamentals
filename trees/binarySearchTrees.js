622f547691ef10a1f8eaa8cc8bc8abc6101bbaec
George McCadden III <thethirdone.g@gmail.com>
2021-02-20 15:41:13 -0800

    Including a lookup method

--- trees/binarySearchTrees.js
+++ trees/binarySearchTrees.js
@@ -12,25 +12,44 @@ class BinarySearchTree {
 	}
 	insert(value) {
 		const newNode = new Node(value);
-		if (this.root === null) {
+		if (!this.root) {
 			this.root = newNode;
-		} else {
-			let curr = this.root;
-			while (true) {
-				if (value < curr.value) {
-					if (!curr.left) {
-						curr.left = newNode;
-						return this;
-					}
-					curr = curr.left;
-				} else {
-					if (!curr.right) {
-						curr.right = newNode;
-						return this;
-					}
-					curr = curr.right;
+			return this;
+		}
+		let curr = this.root;
+		while (true) {
+			if (value < curr.value) {
+				if (!curr.left) {
+					curr.left = newNode;
+					return this;
 				}
+				curr = curr.left;
+			} else if (value > curr.value) {
+				if (!curr.right) {
+					curr.right = newNode;
+					return this;
+				}
+				curr = curr.right;
+			} else if (value === curr.value) {
+				console.log('No duplicates allowed!');
+				break;
 			}
 		}
+	}
+	lookup(value) {
+		if (!this.root) {
+			return false;
+		}
+		let curr = this.root;
+		while (curr) {
+			if (value < curr.value) {
+				curr = curr.left;
+			} else if (value > curr.value) {
+				curr = curr.right;
+			} else if (value === curr.value) {
+				return curr;
+			}
+		}
+		return false;
 	}
 }
