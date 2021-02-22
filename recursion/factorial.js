7fcbda1454a18859a707f72253717a949d82bd3d
George McCadden III <thethirdone.g@gmail.com>
2021-02-22 14:20:11 -0800

    Creating an iterative approach

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,11 @@
 PROBLEM DOMAIN : Write two functions that finds the factorial of any number. One should use recursion and the other should use a 'for loop'.
 
 */
+
+const iterativeFactorial = (num) => {
+	if (num === 0 || num === 1) return 1;
+	for (let i = num - 1; i >= 1; i--) {
+		num *= i;
+	}
+	return num;
+};
