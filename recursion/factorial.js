d98dda800a0cdf70936b714e065987a767e4fa59
George McCadden III <thethirdone.g@gmail.com>
2021-02-22 14:22:01 -0800

    And here comes the recursive approach

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -11,3 +11,8 @@ const iterativeFactorial = (num) => {
 	}
 	return num;
 };
+
+const recursiveFactorial = (num) => {
+	if (num === 0 || num === 1) return 1;
+	return num * recursiveFactorial(num - 1);
+};
