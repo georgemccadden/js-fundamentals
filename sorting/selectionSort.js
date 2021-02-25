a75cdb079967a3cf1b3feca76decf272a72afe62
George McCadden III <thethirdone.g@gmail.com>
2021-02-25 13:41:29 -0800

    Wrote an algorithm that made sense

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -10,5 +10,15 @@ array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 return -> [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]
 
 
-SOLUTION :
+ALGORITHM :
+- Iterate through the array
+- Create variables to keep track of minimum value and temp value
+- For each index of the array, iterate through the array again to update the minimum value
+- If the minimum value of that iteration is less than index you are on, swap it with the minimum
+
+
+SOLUTION:
 */
+const selectionSort = (array) => {
+	const length = array.length;
+};
