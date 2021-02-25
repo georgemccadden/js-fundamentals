ae5ccbd1f8e92e0aa3785536b8271cdddb81778c
George McCadden III <thethirdone.g@gmail.com>
2021-02-25 13:29:14 -0800

    Selection sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,19 +1,16 @@
 /*
 
 PROBLEM DOMAIN :
-
 Implement bubble sort
 
 
 EXAMPLE :
-
 array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 return -> [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]
 
 
 ALGORITHM :
-
 - For every index, iterate through the entire array for comparison
 - If one index is greater than the other, swap and continue
 - If one index is less than the other, continue
---
+++ sorting/selectionSort.js
@@ -0,0 +1,14 @@
+/*
+
+PROBLEM DOMAIN :
+Implement selection sort
+
+
+EXAMPLE :
+array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+return -> [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]
+
+
+SOLUTION :
+*/
