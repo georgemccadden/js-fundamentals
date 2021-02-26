3f6bc6d864fd0029cf84928b11eef2eeafc5c31b
George McCadden III <thethirdone.g@gmail.com>
2021-02-26 00:58:56 -0800

    Just a clean up
    Adding to the examples and algorithm

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -7,8 +7,10 @@ Write an algorithm that performs selection sort.
 EXAMPLE :
 array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-first iteration -> 0 is the min so array = [0, 44, 6, 2, 1, 5, 63, 87, 283, 4, 99];
-second iteration -> 1
+first iteration -> min = 0 so array = [0, 44, 6, 2, 1, 5, 63, 87, 283, 4, 99]
+second iteration -> min = 1 so array = [0, 1, 6, 2, 44, 5, 63, 87, 283, 4, 99]
+third iteration -> min = 2 so array = [0, 1, 6, 2, 44, 5, 63, 87, 283, 4, 99]
+...
 
 
 ALGORITHM :
@@ -16,6 +18,7 @@ ALGORITHM :
 - Create variables to keep track of minimum value and temp value
 - For each index of the array, iterate through the array again to find the the minimum value
 - If the minimum value of that iteration is less than index you are on, swap it with the minimum
+- Return the sorted array once you've reached the end of the array
 
 
 SOLUTION:
