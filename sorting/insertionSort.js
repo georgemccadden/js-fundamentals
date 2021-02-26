1272c7b565d4a304aa802e9c65818704e83c1071
George McCadden III <thethirdone.g@gmail.com>
2021-02-26 00:21:45 -0800

    I will need to revisit this algorithm

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -5,6 +5,18 @@ Write an algorithm that performs insertion sort.
 
 
 EXAMPLE :
+array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+first iteration -> 99 > 44, 6, 2, 1, 5, 63, and 87 but not 283 so array = [44, 6, 2, 1, 5, 63, 87, 99, 283, 4, 0]
+second iteration -> 44 > 6, 2, 1, and 5 but not 63 so array = [6, 2, 1, 5, 44, 63, 87, 99, 283, 4, 0]
+third iteration -> 6 > 2, 1, and 5 but not 44 so array = [2, 1, 5, 6, 44, 63, 87, 99, 283, 4, 0]
 
 
+ALGORITHM :
+- For each index of the array, iterate through the array
+- If the first variable isn't the lowest variable, find where it is lower than the variable to the right of it but greater than the variable to the left.
+- Stop iterating once the array is sorted
+
+
+SOLUTION :
 */
