c1b6f86f4ab4a101ddb35b575e727447f912c52e
George McCadden III <thethirdone.g@gmail.com>
2021-03-01 14:32:52 -0800

    Splitting the array into two

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -21,6 +21,11 @@ SOLUTION :
 
 const mergeSort = (arr) => {
 	if (arr.length === 1) return arr;
+
+	const length = arr.length;
+	const middle = Math.floor(length / 2);
+	const left = arr.slice(0, middle);
+	const right = arr.slice(middle);
 };
 /*
 
