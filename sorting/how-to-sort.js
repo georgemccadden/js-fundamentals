4ff06fd0f511e2fe9971acd22d2fe8ad56fc2fce
George McCadden III <thethirdone.g@gmail.com>
2021-03-01 22:13:41 -0800

    Understanding tradeoffs

---
+++ sorting/how-to-sort.js
@@ -0,0 +1,22 @@
+/*
+
+PROBLEM DOMAINS :
+Determine which sorting algorithm to use given the following circumstances.
+
+Sort 10 schools around your house by distance -> Insertion sort
+
+eBay sorts listings by the current Bid amount -> Radix sort / Counting sort
+
+Sports scores on ESPN -> Quick sort
+
+Massive database (which cannot fit into memory) needs to be sorted through the past year's user data -> Merge sort
+
+Almost sorted Udemy review data needs to update and add 2 new reviews -> Insertion sort / Bubble sort
+
+Temperature records for the past 50 years in Canada -> Radix sort / Counting sort / Quick sort (If the data is in decimal)
+
+Large username database needs to be sorted. (Data is very random) -> Merge sort (If memory isn't too expensive) / Quick sort (If the database isn't too large)
+
+You want to teach sorting for the first time -> Bubble sort / Selection sort
+
+*/
