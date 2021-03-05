7d9d164e52434544ee7501172eb7819523184a56
George McCadden III <thethirdone.g@gmail.com>
2021-03-04 18:04:47 -0800

    Write a clear algorithm

---
+++ searching/bfs.js
@@ -0,0 +1,18 @@
+/*
+
+PROBLEM DOMAIN :
+Write an algorithm that implements breadth first traversal using a binary search tree.
+
+
+ALGORITHM :
+- Keep track of what the current node is, starting at the root
+- Create a list to push node values into
+- Create a queue to keep track of which node to visit next
+- Push the current node (root) into the queue before beginning the traversal
+- While the queue isn't empty ->
+- - Change the current node to the first node in the queue
+- - Push the value of the current node into the list
+- - Check to see if the current node has a left, if so, push it into the queue. If not, check to see if the current node has a right, if so, push it into the queue.
+- Return the list
+
+*/
